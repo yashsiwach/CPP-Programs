4456d4a5a1f06bf0576f4e69a5223b0a781de486
Yash Siwach <112823140+yashulpu@users.noreply.github.com>
2023-01-15 10:10:14 +0000

    done

---
+++ Patterns/p1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int i=1;
+    while(i<=a){
+        int j=a;
+        while (j>=1)
+        {
+            cout<<j;
+            j--;
+        }
+        cout<<endl;
+        i=i+1;
+        
+    }
+}
+/*
+4321
+4321
+4321
+4321*/
