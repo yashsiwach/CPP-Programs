e15bd283a9475e2562fabefff50dfec50627b600
Yash Siwach <112823140+yashulpu@users.noreply.github.com>
2023-01-15 08:11:25 +0000

    Done

--- Prime or not.cpp
+++ Prime or not.cpp
@@ -1,1 +1,21 @@
-#include
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the number";
+    cin>>a;
+    int i=2;
+    int count=0;
+    while(i<a){
+        if (a%i==0){
+            count=1;
+        }
+        i++;
+    }
+    if (count==0){
+        cout<<"yes it is prime"<<endl;
+    }else{
+        cout<<"not prime"<<endl;
+    }
+
+}
