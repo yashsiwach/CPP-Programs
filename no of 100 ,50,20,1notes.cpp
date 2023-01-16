5bd31585526151b135b2ac54e7874f33743647b1
Yash Siwach <112823140+yashulpu@users.noreply.github.com>
2023-01-16 18:56:49 +0000

    basics

---
+++ no of 100 ,50,20,1notes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a;
+    
+    switch (a%100)
+    {
+    case  0 :int z=a/100
+    cout<<<<endl;
+
+            break;
+
+    }
+    switch (a%50)
+    {
+    case 0:
+        break;
+    
+    default:
+        break;
+    }
+}
