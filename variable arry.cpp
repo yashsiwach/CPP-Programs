cd40d55e6061a534630e14130b1bf1c7896f01d0
Yash Siwach <112823140+yashulpu@users.noreply.github.com>
2023-01-22 11:47:34 +0000

    basic

---
+++ variable arry.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void sum(int *ar,int &a){
+    int sum=0;
+    for (int i=0;i<a;i++){
+        sum+=ar[i];
+    }
+    cout<<sum;
+}
+int main(){
+    int a;
+    cin>>a;
+    int *ar=new int [a];
+    for (int i=0;i<a;i++){
+        cin>>ar[i];
+    }
+    sum(ar,a);
+
+}
