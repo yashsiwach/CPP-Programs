3260a129c06c7c3345bac369fc8dc46f5bb55ed7
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-04-17 22:38:32 +0530

    Added Solution - GfG to GitHub

---
+++ School/Second Largest/second-largest.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    
+	    set<int>jat(arr,arr+n);
+	    if(jat.size()<2){
+	        return -1;
+	    }
+	    else{
+	        
+	    
+	    auto it=prev(jat.end(),2);
+	    
+	    
+	    return *(it);
+	    }
+	    // code here}
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
