a3795fb41562b48557a060f14de6163aaf910a62
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-04-26 01:15:48 +0530

    Time: 5 ms (22.68%) | Memory: 7.2 MB (54.28%) - LeetSync

---
+++ average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(),salary.end());
+        int len=salary.size();
+        salary.erase(remove(salary.begin(),salary.end(),salary[0]),salary.end());
+        salary.erase(remove(salary.begin(),salary.end(),salary[len-1]),salary.end());
+        double sum=0;
+        int len2=salary.size();
+        for(auto i:salary)
+        {
+                sum=sum+i;
+
+                
+        }
+        cout<<sum<<" "<<len2<<endl;
+        double ans=sum/len2;
+        return ans;
+        
+    }
+};
