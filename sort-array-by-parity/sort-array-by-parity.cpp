c25b9deb11d232987c4d58062f2d74974ff49300
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-04-26 21:58:39 +0530

    Revised-1

--- sort-array-by-parity/sort-array-by-parity.cpp
+++ sort-array-by-parity/sort-array-by-parity.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int>ans;
+        //sare even to starting m lane h or //odd last m mtlb uske bad me
         for(auto i:nums)
         {
+            //agr even h to phale push krdo vector m
             if(i%2==0) ans.emplace_back(i);
         }
         for(auto j:nums)
         {
+            //agr odd h to even hone k bad dal do inko
             if(j%2!=0) ans.emplace_back(j);
+            
         }
+        //soved moj kro done done !!!!!
         
         return ans;
     }
