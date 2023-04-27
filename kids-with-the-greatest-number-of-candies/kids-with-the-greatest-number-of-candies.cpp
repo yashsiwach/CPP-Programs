2cadbf8284c5c317c2551f1b3ac4aec203552969
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-04-28 00:31:09 +0530

    Revised -1

--- kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        //mtlb jisme extra add krde to vo number list me sabse jada ban jaye
         vector<bool>v;
+        //max element nkl leya sabse
         auto m=max_element(candies.begin(),candies.end());
         int max=*(m);
+        //har 1 me add krke dekhlo ki vo max wale chota h to false krdo 
         for(auto it:candies)
         {
             if(it+extraCandies<max) v.push_back(false);
