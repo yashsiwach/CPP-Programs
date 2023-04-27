c2cef4ae9d64a7d2cb30a4074bb5d7685241266f
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-04-27 20:30:57 +0530

    imp

---  Largest Perimeter Triangle.cpp
+++  Largest Perimeter Triangle.cpp
@@ -21,3 +21,22 @@ public:
         
     }
 };//bruteforece h ye 
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) 
+    {
+        int sum=0;
+        sort(nums.begin(),nums.end());
+        for(int i=nums.size()-3;i>=0;i--)
+        {
+            if(nums[i]+nums[i+1]>nums[i+2])
+            {
+                sum=nums[i]+nums[i+1]+nums[i+2];
+                break;
+            }
+        }return sum;
+
+        
+    }
+};//optimised
