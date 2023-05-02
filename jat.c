93fca9476f3c353b233e5b05858b46ef0eb2d169
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-05-02 17:59:49 +0530

    updatesd

--- jat.c
+++ jat.c
@@ -4,26 +4,8 @@
 int main( )
 {
 
-int *p,n,i;
-printf("Enter the number of blocks we want to reserve:") ;
-scanf("%d",&n) ;
-p=(int*)calloc(n,sizeof(int));
-if(p==NULL)
-{
-printf("Memory not available\n");
-exit(1);
-}
-else
-{
-printf("\n Memory allocation successful");
-printf ("\nEnter integer values: ") ;
-for(i=0;i<n;i++)
-{
-scanf("%d",p+i);
-}
-printf("\n Entered values are:");
-for(i=0;i<n;i++)
-printf("\n%d",*(p+i));
+int *p="world";
+
 
 
 
