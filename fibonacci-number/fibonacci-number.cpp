9617c3ebaf8d7ba6f98c4de090df24e3204b6239
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-05-04 03:48:13 +0530

    Time: 16 ms (22.77%) | Memory: 5.8 MB (94.24%) - LeetSync

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<1)return n;
+        if(n<=1)return n;
         else
         {
-            return abs(fib(n-2)+fib(n-1));
+            return (fib(n-2)+fib(n-1));
         }
         
     }
