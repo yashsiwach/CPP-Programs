f53d0a16f091984d4ecfc0d1040716c1c13dadf2
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-05-05 00:22:59 +0530

    imp

---
+++ reverse a queue.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rev(queue<int>&q)
+{
+    
+    if(q.empty())
+    {
+        return ;
+    }
+    int temp=q.front();
+    q.pop();
+    rev(q);
+    q.push(temp);
+}
+int main()
+{
+    queue<int>q;
+    q.push(2);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(8);
+   
+    rev(q);
+    while(!q.empty())
+    {
+        cout<<q.front()<<endl;
+        q.pop();
+    }
+}
