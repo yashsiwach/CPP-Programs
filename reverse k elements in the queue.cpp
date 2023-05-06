10e891f5f3beb4ae251867148c747cce5eb6fe20
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-05-06 11:10:24 +0530

    imp

---
+++ reverse k elements in the queue.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rev(queue<int>&q,int k)
+{
+    int 
+}
+int main()
+{
+    queue<int>q;
+    int a;
+    cin>>a;
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    q.push(70);
+    rev(q,a);
+}
