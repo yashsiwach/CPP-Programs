315bb9f22dcf794ba0ed8fe3000430c349cc5650
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-05-15 17:36:30 +0530

    Time: 741 ms (7.77%) | Memory: 180.1 MB (87.37%) - LeetSync

--- swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -10,41 +10,32 @@
  */
 class Solution {
 public:
-    // int len(ListNode*head)
-    // {
-    //     int count=0;
-    //     while(head!=NULL)
-    //     {
-    //         count++;
-    //         head=head->next;
-    //     }return count;
-    // }
-    // ListNode* swapNodes(ListNode* &head, int k) {
-    //     ListNode*slow=head;
-    //     ListNode*fast=head;
-    //     int n=len(head);
-    //     for(int i=1;i<k;i++)
-    //     {
-    //         slow=slow->next;
-    //     }
-    //     for(int j=0;j<n-k;j++)
-    //     {
-    //         fast=fast->next;
-    //     }
-    //     cout<<slow->val<<" "<<fast->val;
-    //     swap(slow->val,fast->val);
-    //     return head;
-        
-    // }
-    ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* fast=head,*slow=head,*curr=head;
-        int n=0;
-        while(curr){curr=curr->next;n++;}
-        n=n-k;
-        while(n--)fast=fast->next;
-        k--;
-        while(k--)slow=slow->next;
+    int len(ListNode*head)
+    {
+        int count=0;
+        while(head!=NULL)
+        {
+            count++;
+            head=head->next;
+        }return count;
+    }
+    ListNode* swapNodes(ListNode* &head, int k) {
+        ListNode*slow=head;
+        ListNode*fast=head;
+        int n=len(head);
+        for(int i=1;i<k;i++)
+        {
+            slow=slow->next;
+        }
+        for(int j=0;j<n-k;j++)
+        {
+            fast=fast->next;
+        }
+        cout<<slow->val<<" "<<fast->val;
         swap(slow->val,fast->val);
         return head;
+        
     }
+    
+    
 };
