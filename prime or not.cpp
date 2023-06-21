a378121aa1ae163782ec8c67f0b6d973d0bd455b
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-06-21 15:03:40 +0530

    test

--- prime or not.cpp
+++ prime or not.cpp
@@ -1,17 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-string chack(int a){
-    int flag=0;
-    for (int i=2;i<a;i++) {
-        if(a%i==0) flag=1;
-    }
-    cout<<flag<<endl;
-    if (flag==0) {return "prime";}
-    else {return "not prime";}
-}
 int main(){
     int a;
+    cout<<"enter the number";
     cin>>a;
-    string ans=chack(a);
-    cout<<ans<<endl;
+    int i=2;
+    int count=0;
+    while(i<a){
+        if (a%i==0){
+            count=1;
+        }
+        i++;
+    }
+    if (count==0){
+        cout<<"yes it is prime"<<endl;
+    }else{
+        cout<<"not prime"<<endl;
+    }
+
 }
