e4556e39038aaac815be4cd5564bff99263a55c7
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-06-22 22:32:02 +0530

    imp

--- graph_directed_cycle.cpp
+++ graph_directed_cycle.cpp
@@ -11,21 +11,25 @@ public:
 			gp[e].push_back(s);
 		}
 	}
-	bool check(int src,unordered_map<int,bool>&visited,int parent)
+	bool check(int src,unordered_map<int,bool>&visited,unordered_map<int,bool>&dfs)
 	{
 		visited[src]=true;
+		dfs[src]=true;
 		for(auto it:gp[src])
 		{
 			if(!visited[it])
 			{
-				bool ans=check(it,visited,src);
+				bool ans=check(it,visited,dfs);
 				if(ans==true)
 					return true;
 			}
-			else if (it != parent) { // cycle detected
-     		 return true;
+			 else if (dfs[it] == true) { // cycle detected
+     			 return true;
+    		
     		}
-		}return false;
+		}
+		dfs[src] = false;
+		return false;
 	}
 	
 	
@@ -37,16 +41,17 @@ int main()
 	graph g;
 	g.addedge(0,1,0);
 	g.addedge(1,2,0);
-	g.addedge(2,8,0);
+	g.addedge(2,0,0);
 	g.addedge(3,4,0);
 	g.addedge(4,5,0);
 	g.addedge(5,6,0);
 
 	bool ans = false;
 	unordered_map<int,bool> visited;
+	unordered_map<int,bool> dfs;
 	for (int i = 0; i < 7; i++) {
 	  if (!visited[i])
-	    ans = g.check(i, visited, -1);
+	    ans = g.check(i, visited,dfs);
 	  if (ans == true)
  	   break;
 	}
