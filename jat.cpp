52fb5e0e63b4bb96e849371a88e43e2b3f96c8e5
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-08-18 20:41:11 +0530

    updated

--- jat.cpp
+++ jat.cpp
@@ -1,12 +1,72 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int a[3]={0};
-    for(auto it:a)
+typedef long long ll;
+#define MOD 1000000007
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr<<x<<" ";
+class node{
+public:
+    int roll;
+    int marks;
+    node*next;
+    node()
     {
-        cout<<it;
+        this->roll=0;
+        this->marks=0;
+        this->next=nullptr;
+    }
+    node(int roll,int marks)
+    {
+        this->roll=roll;
+        this->marks=marks;
+        this->next=NULL;
+    }
+
+};
+void print(node*head)
+{
+    node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<"roll "<<temp->roll<<" ";
+        cout<<"marks "<<temp->marks<<endl;
+        temp=temp->next;
     }
 }
 
-//
-//sss
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    clock_t time_req=clock();
+    
+    
+
+    node *first=new node(3,43);
+    node *second=new node(5,35);
+    node *third= new node(7,62);
+    node *forth= new node(7,63);
+    node *fifth= new node(7,67);
+    first->next=second;
+    second->next=third;
+    third->next=forth;
+    forth->next=fifth;
+    fifth->next=nullptr;
+    print(first);
+
+
+
+
+
+#ifndef ONLINE_JUDGE
+    cout<<endl<<"Time: "<<fixed<<setprecision(6)<<((double)(clock()-time_req))/CLOCKS_PER_SEC<<endl;
+#endif
+}
+
