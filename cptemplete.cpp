ae3713d06aa6c1ac08a5924da6d6b1e6b6dccf1d
yashsiwach <yash.12207847@lpu.in>
2023-12-09 17:01:42 +0530

    update

--- cptemplete.cpp
+++ cptemplete.cpp
@@ -1,41 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
+#ifdef ONLINE_JUDGE
+#define DISABLE_STACK_SIZE_CHANGE
+#endif
+#ifndef DISABLE_STACK_SIZE_CHANGE
+#include <sys/resource.h>
+#endif
 typedef long long ll;
+const ll MOD = 1000000007;
+#define pb push_back
+#define sz(x) (ll)x.size()
+#define ff first
+#define ss second
+#define nl "\n"
+#define allr(v) (v).rbegin(),(v).rend()
+unordered_map<int, vector<int>> adj;
+unordered_map<int, bool> visi;
+#define set_bits(x) __builtin_popcountll(x)
+#define ceil(x,y) ceil(double(x)/y)
+#define floor(x,y) floor(double(x)/y)
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr<<#x<<" "<<x<<endl;
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define print(x) for(auto it:(x)) cout<<(it)<<" "; cout<<endl;
+#define showadj for (auto it : adj) { cout << it.ff << " ->"; for (auto i : it.ss) cout << i << " "; cout << endl; }
 
-const int MOD = 1000000007;
+template <typename T>
+T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
 
-ll binpow(ll base, ll power, ll mod) {
+template <typename T>
+T binpow(T base, T power, T mod)
+{
     ll ans = 1;
-    base = base % mod; // Ensure the base is within [0, mod)
-    while (power > 0) {
-        if (power & 1) {
-            ans = (ans * base) % mod;
-        }
-        base = (base * base) % mod;
+    base = base % mod;
+    while (power)
+    {
+        if (power & 1) ans = (ans * base) % mod;
+        base = ((base * base) % mod);
         power >>= 1;
     }
     return ans;
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    vector<ll> v(1000001, 1);
-    for (int i = 1; i < 1000001; i++) {
-        v[i] = (v[i - 1] * i) % MOD;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef DISABLE_STACK_SIZE_CHANGE
+    rlimit rlim;
+    if (getrlimit(RLIMIT_STACK, &rlim) != 0) { return 1; }
+    rlim.rlim_cur = 1024 * 1024 * 1024;
+    if (setrlimit(RLIMIT_STACK, &rlim) != 0) { return 2; }
+#endif
+
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r",stdin);
+    freopen("Output.txt", "w",stdout);
+    freopen("Error.txt", "w",stderr);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
     }
-    string s;
-    cin >> s;
-    int ans = 1;
-    map<char, int> mp;
-    for (auto it : s) {
-        mp[it]++;
-    }
-    for (auto it : mp) {
-        if (it.second > 1) {
-            ans = (ans * binpow(v[it.second], MOD - 2, MOD)) % MOD;
-        }
-    }
-    int final = (ans * v[s.size()]) % MOD;
-    cout << final << endl;
 }
--- jat.cpp
+++ jat.cpp
@@ -1,34 +1,70 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
- 
-const int MAX = 200007;
-const int MOD = 1000000007;
- 
-void solve() {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-    int mp[26];
-    for (int i = 0; i < 26; i++) {
-        mp[i] = -1;
+#ifdef ONLINE_JUDGE
+#define DISABLE_STACK_SIZE_CHANGE
+#endif
+#ifndef DISABLE_STACK_SIZE_CHANGE
+#include <sys/resource.h>
+#endif
+typedef long long ll;
+const ll MOD = 1000000007;
+#define pb push_back
+#define sz(x) (ll)x.size()
+#define ff first
+#define ss second
+#define nl "\n"
+#define allr(v) (v).rbegin(),(v).rend()
+unordered_map<int, vector<int>> adj;
+unordered_map<int, bool> visi;
+#define set_bits(x) __builtin_popcountll(x)
+#define ceil(x,y) ceil(double(x)/y)
+#define floor(x,y) floor(double(x)/y)
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr<<#x<<" "<<x<<endl;
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define print(x) for(auto it:(x)) cout<<(it)<<" "; cout<<endl;
+#define showadj for (auto it : adj) { cout << it.ff << " ->"; for (auto i : it.ss) cout << i << " "; cout << endl; }
+
+template <typename T>
+T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+
+template <typename T>
+T binpow(T base, T power, T mod)
+{
+    ll ans = 1;
+    base = base % mod;
+    while (power)
+    {
+        if (power & 1) ans = (ans * base) % mod;
+        base = ((base * base) % mod);
+        power >>= 1;
     }
-    for (int i = 0; i < n; i++) {
-        int curr = (s[i] - 'a');
-        if (mp[curr] == -1) {
-            mp[curr] = (i % 2);
-        }
-        else {
-            if (mp[curr] != (i % 2)) {cout << "NO\n"; return;}
-        }
-    }
-    cout << "YES\n";
+    return ans;
 }
- 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
-    // solve();
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef DISABLE_STACK_SIZE_CHANGE
+    rlimit rlim;
+    if (getrlimit(RLIMIT_STACK, &rlim) != 0) { return 1; }
+    rlim.rlim_cur = 1024 * 1024 * 1024;
+    if (setrlimit(RLIMIT_STACK, &rlim) != 0) { return 2; }
+#endif
+
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r",stdin);
+    freopen("Output.txt", "w",stdout);
+    freopen("Error.txt", "w",stderr);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+    }
 }
